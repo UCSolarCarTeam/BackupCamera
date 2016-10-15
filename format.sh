987128fbd85f87bea3d099143196d37aa014b5a8
henrytran28 <henrytran28@hotmail.ca>
2016-10-15 00:04:15 -0600

    changed format.sh to not depend on directory

--- format.sh
+++ format.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -e
 
-if [ $# -eq 0 ]; then 
-	echo "Pass directory to format as argument"
-	echo "Example: ./format.sh src"
-	exit 1
-fi
-
-astyle "$1/*.h" "$1/*.c" -r --options=astylerc
-find $1 -name "*.orig" -delete
+astyle "*.h" "*.c" -r --options=astylerc
+find -name "*.orig" -delete
