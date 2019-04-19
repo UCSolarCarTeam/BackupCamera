feeb8e8c4a8b3b1bc8341d2fb4aef54832f0e03f
Bill Luu <billluu619@gmail.com>
2019-04-19 15:02:58 -0600

    Add lock file implementation to check single runner
    User QLock to see if an instance of the backup camera is already existing, similar to Hermes or the Dashboard.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL.h>
+#include <QLockFile>
+#include <QDebug>
+#include <QDir>
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +13,26 @@ int main(int argc, char* argv[])
         printf("Usage: %s   screen_x_cordinate  screen_y_coordinate  screen_width  screen_height  camera_res_x  camera_res_y\n", argv[0]);
         return 0;
     }
+
+    QString tmpDir = QDir::tempPath();
+    QLockFile lockFile(tmpDir + "/BackupCamera.lock");
+    /* Trying to close the Lock File, if the attempt is unsuccessful for 100 milliseconds,
+     * then there is a Lock File already created by another process.
+     * Therefore, we issue a warning and close the program
+     */
+    if (!lockFile.tryLock(100))
+    {
+        qDebug() << "An instance of Backup Camera already exists.";
+        qDebug() << "If you are sure you only have one instance of Backup Camera running, please delete the file /tmp/BackupCamera.lock as root.";
+        qDebug() << "Quitting...";
+        return 1;
+    }
+    else
+    {
+        qDebug() << "No other instance of Hermes exists.";
+        qDebug() << "Launching Hermes...";
+    }
+
 
     BackupCamera* backupCamera = new BackupCamera();
     SDL_Renderer* renderer = NULL;
